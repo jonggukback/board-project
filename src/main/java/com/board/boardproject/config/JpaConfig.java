12f9d35ece81d2c008ccd3da560896314c266049
jongGuk <qorwhdr@naver.com>
2023-04-10 22:14:03 +0900

    #11 -  Spring Data JPA 속성 설정

---
+++ src/main/java/com/board/boardproject/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.board.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("baek"); // TODO: 스프링 시큐리티 추가시 수정할 사항입니다.
+    }
+}
