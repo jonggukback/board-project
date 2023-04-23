deb2c28219328f0705eaecabfb345870c87ecea9
jongGuk <qorwhdr@naver.com>
2023-04-23 17:37:38 +0900

    # 21 - Root 페이지 컨트롤러 및 테스트 생성

---
+++ src/test/java/com/board/boardproject/controller/MainControllerTest.java
@@ -0,0 +1,33 @@
+package com.board.boardproject.controller;
+
+import com.board.boardproject.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(MainController.class)
+@Import(SecurityConfig.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void given_when_then() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
